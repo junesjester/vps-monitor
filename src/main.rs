2caa4aad6a286300dbb4ab6a8e5eb4727acec173
jester <sccrane2005@gmail.com>
2026-09-22 20:49:07 -0500

    new

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod config;
 mod checker;
 
+use config::CheckType;
+
 fn main() {
     let cfg = config::load("config.toml");
+
     for service in &cfg.service {
-        println!("{}: checking...", service.name);
+        let up = match service.check {
+            CheckType::Port => checker::check_port(&service.target),
+            CheckType::Http => {
+                println!("{}: http check not implemented yet", service.name);
+                false
+            }
+            CheckType::Process => {
+                println!("{}: process check not implemented yet", service.name);
+                false
+            }
+        };
+        println!("{}: {}", service.name, if up { "UP" } else { "DOWN" });
     }
-    let up = checker::check_port("127.0.0.1:80");
-    println!("nginx up: {}", up);
 }
